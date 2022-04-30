76323149c5eed829ee37323117695054b2c6be39
santosh33 <santosh33@gmail.com>
2022-05-01 01:28:44 +0530

    Configure Axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid';
+import agent from "../../api/agent";
 function App() {
   const[activities, setActivities] = useState<Activity[]>([]);
   const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get<Activity[]>('http://localhost:5000/api/activities').then(response=> {
-      setActivities(response.data);
+    agent.Activities.list().then(response=> {
+      setActivities(response);
     })
   },[])
 
