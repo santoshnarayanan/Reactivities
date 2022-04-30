da47844c89f9b516ef448ddbdc710e7f5f4e5a32
santosh33 <santosh33@gmail.com>
2022-05-01 02:34:34 +0530

    changes to http

--- client-app/src/api/agent.ts
+++ client-app/src/api/agent.ts
@@ -1,6 +1,7 @@
 import axios, {AxiosResponse} from "axios";
 
-axios.defaults.baseURL = "https://localhost:5000/api";
+//axios.defaults.baseURL = "https://localhost:5000/api";
+axios.defaults.baseURL = "http://localhost:5000/api";
 
 const responseBody = (response:AxiosResponse) => response.data;
 
