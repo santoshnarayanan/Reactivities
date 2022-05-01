c9d6dae932072f5de2c757f049503d56e7cb9ab7
santosh33 <santosh33@gmail.com>
2022-05-01 08:24:54 +0530

    Posting data on server

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,77 +3,91 @@ import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
-import {v4 as uuid} from 'uuid';
+import { v4 as uuid } from 'uuid';
 import agent from "../../api/agent";
 import LoadingComponent from './LoadingComponent';
 function App() {
-  const[activities, setActivities] = useState<Activity[]>([]);
-  const[selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
   const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.Activities.list().then(response=> {
-      let activities: Activity [] = [];
-      response.forEach(activity =>{
+    agent.Activities.list().then(response => {
+      let activities: Activity[] = [];
+      response.forEach(activity => {
         activity.date = activity.date.split('T')[0];
         activities.push((activity));
       })
       setActivities(activities);
       setLoading(false);
     })
-  },[])
+  }, [])
 
-  function handleDeleteActivity(id:string){
-    setActivities([...activities.filter(x=>x.id !== id)]);
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter(x => x.id !== id)]);
   }
 
-  function handleSelectActivity(id:string){
-    setSelectedActivity(activities.find(x=> x.id === id));
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find(x => x.id === id));
   }
 
-  function handleCancelSelectedActivity(){
+  function handleCancelSelectedActivity() {
     setSelectedActivity(undefined);
   }
 
-  function handleFormOpen(id?: string){
+  function handleFormOpen(id?: string) {
     id ? handleSelectActivity(id) : handleCancelSelectedActivity();
     setEditMode(true);
   }
 
-  function handleFormClose(){
+  function handleFormClose() {
     setEditMode(false);
   }
 
-  function handleCreateOrEditActivity(activity: Activity){
-    activity.id ?
-        setActivities([...activities.filter(x=>x.id !== activity.id), activity])
-        : setActivities([...activities, {...activity, id:uuid()}]);
+  function handleCreateOrEditActivity(activity: Activity) {
+    setSubmitting(true);
+    if (activity.id) {
+      agent.Activities.update(activity).then(() => {
+        setActivities([...activities.filter(x => x.id !== activity.id), activity])
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
+    } else {
+      activity.id = uuid();
+      agent.Activities.create(activity).then(() => {
+        setActivities([...activities, activity]);
+        setSelectedActivity(activity);
+        setEditMode(false);
+        setSubmitting(false);
+      })
 
-    setEditMode(false);
-    setSelectedActivity(activity);
+    }
   }
 
 
-  if(loading) return <LoadingComponent content='Loading app'/>
+  if (loading) return <LoadingComponent content='Loading app' />
 
-   return (
+  return (
     <>
-      <NavBar openForm={handleFormOpen}/>
-      <Container style={{marginTop:'7em'}}>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
           activites={activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectActivity}
           cancelSelectActivity={handleCancelSelectedActivity}
           editMode={editMode}
-          openForm ={handleFormOpen}
+          openForm={handleFormOpen}
           closeForm={handleFormClose}
           createOrEdit={handleCreateOrEditActivity}
-          deleteActivity = {handleDeleteActivity}
-          />Remo
+          deleteActivity={handleDeleteActivity}
+          submitting={submitting}
+        />
       </Container>
-       
+
     </>
   );
 }
