5502ce3142a52a58859c948bbe4cb69cbdd2f9e0
santosh33 <santosh33@gmail.com>
2022-05-05 00:06:50 +0530

    Formatting document

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Container} from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import NavBar from './NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
@@ -7,10 +7,10 @@ import { v4 as uuid } from 'uuid';
 import agent from "../../api/agent";
 import LoadingComponent from './LoadingComponent';
 import { useStore } from '../stores/store';
-import {observer} from "mobx-react-lite";
+import { observer } from "mobx-react-lite";
 
 function App() {
-  const {activityStore} = useStore();
+  const { activityStore } = useStore();
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
   const [editMode, setEditMode] = useState(false);
@@ -23,7 +23,7 @@ function App() {
 
   function handleDeleteActivity(id: string) {
     setSubmitting(true);
-    agent.Activities.delete(id).then(()=>{
+    agent.Activities.delete(id).then(() => {
       setActivities([...activities.filter(x => x.id !== id)])
       setSubmitting(false);
     })
