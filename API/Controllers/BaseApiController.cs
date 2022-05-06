0d46aa0f9aa74392e33f4a4cb6729744be71ffd9
santosh33 <santosh33@gmail.com>
2022-05-06 20:01:27 +0530

    Handling API response with Result Generic Object

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,4 +1,5 @@
-﻿using MediatR;
+﻿using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,20 @@ namespace API.Controllers
         private IMediator _mediator;
 
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result.IsSuccess && result.Value != null)
+            {
+                return Ok(result.Value);
+            }
+            else if (result.IsSuccess && result.Value == null)
+            {
+                return NotFound();
+            }
+            else return BadRequest(result.Error);
+        }
 
     }
 }
